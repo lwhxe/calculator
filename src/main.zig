498680e60cee2acb77f891ee32ad3fecce536bfa
lwhxe <alexanderthomasbohman@gmail.com>
2025-08-19 22:26:18 +0200

    initial parsing

--- src/main.zig
+++ src/main.zig
@@ -1,46 +1,33 @@
-//! By convention, main.zig is where your main function lives in the case that
-//! you are building an executable. If you are making a library, the convention
-//! is to delete this file and start with root.zig instead.
-
-pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // Don't forget to flush!
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "use other module" {
-    try std.testing.expectEqual(@as(i32, 150), lib.add(100, 50));
-}
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
-}
-
 const std = @import("std");
 
-/// This imports the separate module containing `root.zig`. Take a look in `build.zig` for details.
-const lib = @import("calculator_lib");
+fn get_operation() void {}
+
+fn evaluate(expression: []const u8) !f64 {
+    const iter = std.mem.tokenizeAny(u8, expression, " ");
+    while (iter.next()) |part| {
+        if (part.len == 1) {
+        } else {
+        }
+    }
+
+    return;
+}
+
+pub fn main() !void {
+    const args = try std.process.argsAlloc(std.heap.page_allocator);
+    if (args.len < 2) {
+        for (args, 0) |expression, i| {
+            if (i < 2) continue;
+            if (i > 5) {
+                std.debug.print("\n\nChill out a little. You don't need this many expressions.\n\n", .{});
+                return;
+            }
+            try evaluate(expression);
+        }
+    } else {
+        std.debug.print("No mathematical expression detected.\n\n", .{});
+        return;
+    }
+
+    return;
+}
