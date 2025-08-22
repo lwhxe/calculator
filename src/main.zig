cdde8bc7b59b1a1976a3794f9f0343ed240006bf
UrLastWish <149725738+lwhxe@users.noreply.github.com>
2025-08-22 13:03:30 +0200

    variable content error fixed

--- src/main.zig
+++ src/main.zig
@@ -1,19 +1,19 @@
 const std = @import("std");
 
-fn operator_bind_power(operand: method) !u8 {
+fn operator_bind_power(operand: method) u8 {
     switch (operand.operator) {
         '*' => { return 3; },
         '/' => { return 3; },
         '-' => { return 1; },
         '+' => { return 1; },
-        else => { std.debug.print("Syntax Error.\n\n", .{}); return error.syntax; }
+        else => { return 0; }
     }
 
     return 0;
 }
 
-fn calculate(operand: method, lhs: f64, rhs: f64) !f64 {
-    return switch (operand.operator) {
+fn calculate(operand: u8, lhs: f64, rhs: f64) !f64 {
+    return switch (operand) {
         '*' => { return lhs * rhs; },
         '/' => { return lhs / rhs; },
         '-' => { return lhs - rhs; },
@@ -22,16 +22,17 @@ fn calculate(operand: method, lhs: f64, rhs: f64) !f64 {
     };
 }
 
+// TODO: Fix calculation statements.
 fn parse_expression(tokens: []const method, bp: u8, index: usize) !f64 {
-    if (tokens.len == index) return tokens[index].value;
+    if (tokens.len <= index + 2) return tokens[index].value;
 
     const lhs = tokens[index].value;
 
-    const cbp = try operator_bind_power(tokens[index + 1]);
-    if (bp + 1 > cbp) {
-        return calculate(tokens[index + 1], lhs, tokens[index + 2].value);
+    const cbp = operator_bind_power(tokens[index + 1]);
+    if (bp + 1 < cbp) {
+        return calculate(tokens[index + 1].operator, lhs, tokens[index + 2].value);
     } else {
-        return calculate(tokens[index + 1], lhs, try parse_expression(tokens, cbp, index + 2));
+        return calculate(tokens[index + 1].operator, lhs, try parse_expression(tokens, cbp, index + 2));
     }
 }
 
@@ -45,14 +46,11 @@ const method = union(method_tag) {
     value: f64,
 };
 
-fn evaluate(expression: []const u8) !f64 {
-    if (expression.len == 0) {
+fn evaluate(expr: []const u8) !f64 {
+    if (expr.len == 0) {
         return 0;
     }
-    var buf_e: [100]u8 = undefined;
     var buf_m: [100]method = undefined;
-    const len = std.mem.replace(u8, expression, " ", "", &buf_e);
-    const expr = buf_e[0..len];
 
     for (expr, 0..) |part, i| {
         if (part >= '0' and part <= '9') {
@@ -63,12 +61,15 @@ fn evaluate(expression: []const u8) !f64 {
                 '/' => { buf_m[i] = method{ .operator = part }; },
                 '-' => { buf_m[i] = method{ .operator = part }; },
                 '+' => { buf_m[i] = method{ .operator = part }; },
+                ' ' => continue,
                 else => { std.debug.print("Syntax Error.\n\n", .{}); }
             }
         }
     }
 
     const expr_method = buf_m[0..expr.len];
+    std.debug.print("{s}\n", .{expr});
+    std.debug.print("{any}\n", .{expr_method});
     return try parse_expression(expr_method, 0, 0);
 }
 
@@ -76,7 +77,7 @@ pub fn main() !void {
     const args = try std.process.argsAlloc(std.heap.page_allocator);
     if (args.len > 1) {
         for (args, 0..) |expression, i| {
-            if (i == 1) continue;
+            if (i == 0) continue;
             if (i > 5) {
                 std.debug.print("\n\nChill out a little. You don't need this many expressions.\n\n", .{});
                 return;
