b39936f2e73f393a8313a105d562b0f575108945
UrLastWish <bohmanalexander09@gmail.com>
2025-09-11 15:48:17 +0200

    fix error messages

--- src/main.zig
+++ src/main.zig
@@ -4,7 +4,7 @@ fn prompt() !f64 {
     const stdin = std.io.getStdIn();
     var buffer: [100]u8 = undefined;
     std.debug.print("Please input a numeric value: ", .{});
-    return try std.fmt.parseFloat(f64, try nextLine(stdin.reader(), &buffer) orelse "");
+    return std.fmt.parseFloat(f64, try nextLine(stdin.reader(), &buffer) orelse "") catch { std.debug.print("\x1b[A\x1b[2K\x1b[31mInvalid Character; \x1b[0m", .{}); return error.InvalidCharacter; };
 }
 
 fn nextLine(reader: anytype, buffer: []u8) !?[]const u8 {
@@ -20,10 +20,7 @@ pub fn main() !void {
     defer stdin.close();
     var buffer: [100]u8 = undefined;
 
-    std.debug.print(\\Welcome to the calculator.
-                    \\You will be prompted to input values and operands, until you input \"n\" or \"stop\".
-                    \\
-                    , .{});
+    std.debug.print("Welcome to the calculator.\nYou will be prompted to input values and operands, until you input \x1b[32m\"n\"\x1b[0m as the operand.\n\n" , .{});
 
     var total: f64 = undefined;
     std.debug.print("Please input a numeric value: ", .{});
@@ -31,28 +28,29 @@ pub fn main() !void {
     total = try std.fmt.parseFloat(f64, first_value_string);
 
     while (true) {
-        std.debug.print("Which operand would you like to use? (+, -, *, /) ", .{});
+        std.debug.print("Which operand would you like to use? (+, -, *, /) \x1b[32m(\"n\" to quit)\x1b[0m ", .{});
         const operand_string = try nextLine(stdin.reader(), &buffer) orelse "";
 
         p: switch (operand_string[0]) {
             '*' => {
-                total *= try prompt();
+                total *= prompt() catch { continue :p operand_string[0]; };
             },
             '/' => {
-                const div = try prompt();
+                const div = prompt() catch { continue :p operand_string[0]; };
                 if (div == 0) {
-                    std.debug.print("Division by zero is not possible.\n", .{});
+                    std.debug.print("\x1b[A\x1b[2K\x1b[31mDivision by zero; \x1b[0m", .{});
                     continue :p '/';
                 }
                 total /= div;
             },
             '+' => {
-                total += try prompt();
+                total += prompt() catch { continue :p operand_string[0]; };
             },
             '-' => {
-                total -= try prompt();
+                total -= prompt() catch { continue :p operand_string[0]; };
             },
-            else => { break; },
+            'n' => { break; },
+            else => { std.debug.print("\x1b[A\x1b[2K\x1b[31mInvalid Operand; \x1b[0m", .{}); continue; },
         }
 
         std.debug.print("Current total: {d}\n", .{total});
